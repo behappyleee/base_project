fc1fcf81dc90b1f8adc3ba0731764b16bddf0030
behappyleee <dhfhfkwhdk@naver.com>
2023-06-14 00:10:05 +0900

    Master Branch commit

--- java/java_server/src/SimpleHTTPServer.java
+++ java/java_server/src/SimpleHTTPServer.java
@@ -3,9 +3,15 @@ import java.net.ServerSocket;
 public class SimpleHTTPServer {
 
     public static void main(String[] args) throws Exception {
-        // 8080 Listening ServerSocket 생성
+        // Listening 8080 port ServerSocket 생성
         final ServerSocket serverSocket = new ServerSocket(8080);
+
+        // Acception Connection
         System.out.println("Listening for connection on port 8080 ....");
+
+        // TODO
+        // https://javarevisited.blogspot.com/2015/06/how-to-create-http-server-in-java-serversocket-example.html#axzz84WaetD1p
+        // Accption Code 부터 다시 보기 !!!
 
         while(true) {
             if(true) {
