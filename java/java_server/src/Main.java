de5aa1c45eca1ad76af05f0b16ca2033d49dd1e8
behappyleee <dhfhfkwhdk@naver.com>
2023-06-13 23:58:57 +0900

    Initial Project commit

---
+++ java/java_server/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Java HTTP Server
+        // Java is using TCP Sockets.
+        // Create a simple HTTP Server.
+
+
+
+    }
+}
---
+++ java/java_server/src/SimpleHTTPServer.java
@@ -0,0 +1,18 @@
+import java.net.ServerSocket;
+
+public class SimpleHTTPServer {
+
+    public static void main(String[] args) throws Exception {
+        // 8080 Listening ServerSocket 생성
+        final ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+
+        while(true) {
+            if(true) {
+                break;
+            }
+        }
+
+    }
+
+}
