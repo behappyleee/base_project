0cc8f1be8c6ac603228ebe4acdcbdabfa5b5669a
behappyleee <dhfhfkwhdk@naver.com>
2023-07-11 00:03:25 +0900

    bean studying commit

---
+++ springboot/bean/src/test/java/com/spring/bean/BeanProcessTest.java
@@ -0,0 +1,31 @@
+package com.spring.bean;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeanProcessTest {
+
+    static class BeanClassTest {
+
+        @PreDestroy
+        public void preDestroyMethod() {
+            System.out.println("THIS IS PRE DESTROY ANNOTATION METHOD !!");
+        }
+
+        @PostConstruct
+        public void postConstructMethod() {
+            System.out.println("THIS IS POST CONSTRUCT ANNOTATION METHOD !!");
+        }
+
+    }
+
+    @Test
+    public void beanProcessTest() {
+        BeanClassTest bb = new BeanProcessTest.BeanClassTest();
+
+    }
+
+}
