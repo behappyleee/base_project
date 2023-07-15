d2ad73836ac479b005b721853658e4994f12f86a
behappyleee <dhfhfkwhdk@naver.com>
2023-07-15 18:25:56 +0900

    solve import error

--- springboot/spring-webflux/src/main/java/com/spring/webflux/tutorial/router/RouterConfig.java
+++ springboot/spring-webflux/src/main/java/com/spring/webflux/tutorial/router/RouterConfig.java
@@ -1,26 +1,29 @@
 package com.spring.webflux.tutorial.router;
 
+//import com.spring.webflux.tutorial.handler.UserHandler;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.ServerResponse;
+//import static org.springframework.web.servlet.function.RequestPredicates.*;
+//import static org.springframework.web.servlet.function.RouterFunctions.route;
+
 import com.spring.webflux.tutorial.handler.UserHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-
-import static org.springframework.web.servlet.function.RequestPredicates.*;
-import static org.springframework.web.servlet.function.RouterFunctions.route;
-
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class RouterConfig {
 
     @Bean
     RouterFunction <ServerResponse> routes(UserHandler handler) {
-        // TODO
-        // 확인 필요
-        // 왜 geAllUsers 에러 발생 하는 지 .... ???
         return route(GET("/handler/users").and(accept(MediaType.APPLICATION_JSON)), handler::getAllUsers)
                 .andRoute(GET("/handler/users/{userId}").and(contentType(MediaType.APPLICATION_JSON)), handler::getUserById)
                 .andRoute(POST("/handler/users").and(accept(MediaType.APPLICATION_JSON)), handler::create)
