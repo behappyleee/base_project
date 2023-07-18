d48d25425ae228b91c396c3be3bff9a54fc28e64
behappyleee <dhfhfkwhdk@naver.com>
2023-07-18 23:43:01 +0900

    make sample project

---
+++ springboot/exception/src/main/java/com/handling/exception/ExceptionApplication.java
@@ -0,0 +1,17 @@
+package com.handling.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionApplication {
+
+	// SpringBoot 에서 ExceptionHandling
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionApplication.class, args);
+	}
+
+}
---
+++ springboot/kafka/src/main/java/com/sample/kafka/KafkaApplication.java
@@ -0,0 +1,17 @@
+package com.sample.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+	// TODO
+	// 해당 자료 보고 학습 필요 !!
+	// https://reflectoring.io/spring-boot-kafka/
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaApplication.class, args);
+	}
+
+}
