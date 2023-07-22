155fd78ada090de2dc5a1047826cb1c3daacf10a
behappyleee <dhfhfkwhdk@naver.com>
2023-07-23 01:39:59 +0900

    Thread Studying commit

--- java/java_thread/src/Thread_Control_2.java
+++ java/java_thread/src/Thread_Control_2.java
@@ -1,10 +1,14 @@
 import java.math.BigInteger;
 
 public class Thread_Control_2 {
-    public static void main(String[] args) {
-        Thread thread = new Thread(new LongComputationTask(new BigInteger("2"), new BigInteger("10")));
 
+    public static void main(String[] args) {
+        // Thread thread = new Thread(new LongComputationTask(new BigInteger("2"), new BigInteger("10")));
+        // 만약 엄청나게 큰 숫자를 사용자가 입력 하는 경우
+        // 계산이 끝날 때 까지 기다리거나 Thread 를 Interrupt 하여 쓰레드를 종료 하여야 함
+        Thread thread = new Thread(new LongComputationTask(new BigInteger("20000"), new BigInteger("10000000")));
         thread.start();
+        thread.interrupt(); // Interrupt 를 할 시 Interrupt 시 처리하여야 할 코드가 있어여 함 !
     }
 
     private static class LongComputationTask implements Runnable {
@@ -23,14 +27,17 @@ public class Thread_Control_2 {
 
         private BigInteger pow(BigInteger base, BigInteger power) {
             BigInteger result = BigInteger.ONE;
+            // Interrupt 를 처리하기 위하여서는 Code 실행 시 오래걸리는 부분을 찾아야 함
+            // 현재는 해당 for 문 Loop 에서 오래 걸림
             for(BigInteger i=BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+                if(Thread.currentThread().isInterrupted()) {
+                    System.out.println("Permaturely interrupted computation !");
+                    return BigInteger.ZERO;
+                }
                 result = result.multiply(base);
             }
-
             return result;
         }
-
     }
-
 
 }
---
+++ java/java_thread/src/Thread_Daemon.java
@@ -0,0 +1,12 @@
+public class Thread_Daemon {
+
+    public static void main(String[] args) {
+
+        // Daemon Thread 는 배경에서 실행 되는 스레드로
+        // 메인 스레드가 종료되어도 어플리케이션 종료를 막지 않음
+        // 몇몇 시나리오에서 스레드를 Daemon 으로 생성을하면 앱의 주 작업이 아닌 백그라운드 작업을 맡음
+
+
+    }
+
+}
