439ccb75ee3ea7e29d882ec0bdc43391f5338729
behappyleee <dhfhfkwhdk@naver.com>
2023-08-04 23:55:46 +0900

    Thread Studying commit

--- java/java_thread/src/ThreadOrder.java
+++ java/java_thread/src/ThreadOrder.java
@@ -12,9 +12,13 @@ public class ThreadOrder {
     // Thread A 가 다 끝났는 지 확인 하기 위하 여서는  아주 단순한 방법 으로는 무한 루프를 돌리는 것이다. 계속 무한 루프를 돌리면서
     // Thread A 가 작업이 다 마쳤는 지 계속 확인 하는 것 이지만 이는 아주 비효율 적이다.
     // 이 때 사용하는 것이 join() 이다.
-
-    public static void main(String[] args) {
-        List<Long> inputNumbers = Arrays.asList(0L, 3435L, 35435L, 2324L, 4656L, 23L, 2435L, 5566l);
+        
+    // Thread join 을 통하여 위에 Thread 가 끝날 떄까지 기다림
+    
+    public static void main(String[] args) throws InterruptedException {
+        // List<Long> inputNumbers = Arrays.asList(0L, 3435L, 35435L, 2324L, 4656L, 23L, 2435L, 5566l);
+        
+        List<Long> inputNumbers = Arrays.asList(100000000L, 3435L, 35435L, 2324L, 4656L, 23L, 2435L, 5566l);
 
         // 계승을 계산하는 것은 아주 Heavy 한 작업 임
         // 그래서 다른 Thread 에 맡김
@@ -28,9 +32,20 @@ public class ThreadOrder {
         }
 
         for(Thread thread : threads) {
+            thread.setDaemon(true);
             thread.start();
         }
-    
+            
+        // Thrad Join 을 통하여 Thread 를 더 강력하게 통제가 가능함
+        // Main 쓰레드 Loop 종료 될 떄 까지 모두 계승 Thread 는 작업을 마치게 됨
+        
+        // 믿을만한 결과를 만들기 위하여서는 늘 스레드를 조정하야여 함
+        // 항상 방어적 프로그래밍을 해야함
+        for(Thread thread : threads) {
+           // thread.join();
+            thread.join(2000);  // 모든 쓰레드의 작업이 2초를 넘치 않게 설정 함
+        }
+
         // 동기적으로 실행 하여 계산이 짧은 쓰레드는 끝나있음
         // Thread 는 동시에 시작하였지만 끝나는 것은 작업 속도에 따라 모든 Thread 가 다름 !!!!
         for(int i=0; i<inputNumbers.size(); i++) {
