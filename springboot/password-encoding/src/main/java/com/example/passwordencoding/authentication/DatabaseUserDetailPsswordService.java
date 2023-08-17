6424ecf6d4218a9a3bbe94dc0507050b5b073ab9
behappyleee <dhfhfkwhdk@naver.com>
2023-08-17 23:46:50 +0900

    Spring Security Studying commit

---
+++ springboot/password-encoding/src/main/java/com/example/passwordencoding/authentication/DatabaseUserDetailPsswordService.java
@@ -0,0 +1,32 @@
+package com.example.passwordencoding.authentication;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class DatabaseUserDetailPsswordService implements UserDetailsPasswordService {
+
+    private final UserRepository userRepository;
+
+    private final UserDetailMapper userDetailMapper;
+
+
+    public DatabaseUserDetailPsswordService(UserRepository userRepository, UserDetailMapper userDetailMapper) {
+        this.userRepository = userRepository;
+        this.userDetailMapper = userDetailMapper;
+    }
+
+    @Override
+    public UserDetails updatePassword(UserDetails user, String newPassword) {
+        UserCredentials userCredentials = userRepository.findByUsername(user.getUsername());
+        userCredentials.setPassword(newPassword);
+
+        return userDetailMapper.toUserDetails(userCredentials);
+    }
+
+
+
+}
---
+++ springboot/password-encoding/src/main/java/com/example/passwordencoding/authentication/UserDetailMapper.java
@@ -0,0 +1,20 @@
+package com.example.passwordencoding.authentication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailMapper {
+
+    Logger logger = LoggerFactory.getLogger(UserDetailMapper.class);
+
+    UserDetails toUserDetails(UserCredentials userCredentials) {
+        return User.withUsername(userCredentials.getPassword())
+                .roles(userCredentials.getRoles().toArray(String[]::new))
+                .build();
+    }
+
+}
