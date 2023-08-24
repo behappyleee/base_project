54268037668344cf3722f8abe92fe42405b1c43e
behappyleee <dhfhfkwhdk@naver.com>
2023-08-24 20:06:37 +0900

    Spring boot actuator commit

--- springboot/actuator/src/main/java/com/sample/actuator/ActuatorApplication.java
+++ springboot/actuator/src/main/java/com/sample/actuator/ActuatorApplication.java
@@ -2,12 +2,38 @@ package com.sample.actuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class ActuatorApplication {
 
+	// Actuator 설정 시 노출 엔드 포인트를 통하여 모니터링 가능한 데이터를 제공
+	// Springboot Actuator 는 SpringBoot 하위 프레임워크 프로젝트임
+	// SpringBoot Actuator 는 어플리케이션을 모니터링하고 관리하는데 도움이 되는 여러가지 추가 기능이 포함되어 있음
+	// HTTP 및 JMX 엔드 포인트를 사용하여 SpringBoot 어플리케이션을 관리하고 모니터링이 가능함
+
+	// SpringBoot Actuator 3가지 중요한 개념
+
+	// 1. Endpoints
+	// Actuator 끝 점을 통하여 Application 모니터링이 가능한 application.properties 에서 설정 한 노출 Endpoint URL 을 통하여 접근 가능
+	// ex) health 보고 싶을 시 /actuator/health Endpoint 로 접근
+
+	// 2. Metrics
+	// Application 에 핵심이 되는 정보들 이 어플리케이션이 사용 되는 메모리, CPU 가 어느 정도 되는 지 그런 정보들을 여러 모니터링 어플리케이션
+	// 사용할 수 있는 공통의 포맷을 만들어서 제공
+
+
+	// 3. Audit
+
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(ActuatorApplication.class, args);
+		SpringApplication app = new SpringApplication(ActuatorApplication.class);
+		// SpringApplication 시작 시 시간 살짞 늦춰 줌
+		app.setApplicationStartup(new BufferingApplicationStartup(2048));
+		app.run(args);
+
+		// SpringApplication.run(ActuatorApplication.class, args);
 	}
 
 }
--- springboot/actuator/src/main/java/com/sample/actuator/controller/OrderController.java
+++ springboot/actuator/src/main/java/com/sample/actuator/controller/OrderController.java
@@ -1,4 +1,74 @@
 package com.sample.actuator.controller;
 
+import com.sample.actuator.model.Order;
+import com.sample.actuator.model.OrderCreatedResponse;
+import com.sample.actuator.model.OrderLineItem;
+import com.sample.actuator.services.OrderService;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private MeterRegistry registry;
+
+
+    @GetMapping("/{customerId}/orders")
+    public List<Order> getOrders(@PathVariable(value = "customerId") String customerId) {
+        System.out.println("Get orders requested for customer: " + customerId);
+
+        // Dummy order history for example purpose
+        OrderLineItem orderLineItem1 = new OrderLineItem();
+        orderLineItem1.setProductId(UUID.randomUUID().toString());
+        orderLineItem1.setQuantity(10);
+
+        OrderLineItem orderLineItem2 = new OrderLineItem();
+        orderLineItem2.setProductId(UUID.randomUUID().toString());
+        orderLineItem2.setQuantity(12);
+
+        OrderLineItem orderLineItem3 = new OrderLineItem();
+        orderLineItem3.setProductId(UUID.randomUUID().toString());
+        orderLineItem3.setQuantity(5);
+
+
+        OrderLineItem orderLineItem4 = new OrderLineItem();
+        orderLineItem4.setProductId(UUID.randomUUID().toString());
+        orderLineItem4.setQuantity(25);
+
+        OrderLineItem orderLineItem5 = new OrderLineItem();
+        orderLineItem5.setProductId(UUID.randomUUID().toString());
+        orderLineItem5.setQuantity(20);
+
+        OrderLineItem orderLineItem6 = new OrderLineItem();
+        orderLineItem6.setProductId(UUID.randomUUID().toString());
+        orderLineItem6.setQuantity(4);
+
+        return Arrays.asList(new Order(customerId, Arrays.asList(orderLineItem1, orderLineItem2)),
+                new Order(customerId, Arrays.asList(orderLineItem3)),
+                new Order(customerId, Arrays.asList(orderLineItem4, orderLineItem5, orderLineItem6)));
+    }
+
+    @PostMapping("/{customerId}/orders")
+        public OrderCreatedResponse placeOrder(@PathVariable(value = "customerId") String customerId,
+                @RequestBody Order order) {
+            System.out.println("Place order requested for customer: " + customerId);
+
+            registry.counter("orders.placed.counter").increment();
+
+            OrderCreatedResponse response = new OrderCreatedResponse();
+            response.setOrderId(UUID.randomUUID().toString());
+            response.setCustomerId(customerId);
+
+        return response;
+    }
+
 }
