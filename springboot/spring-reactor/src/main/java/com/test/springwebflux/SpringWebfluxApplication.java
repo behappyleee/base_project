c08296689dec3a11da90d6b45b369b386e18bffb
behappyleee <dhfhfkwhdk@naver.com>
2023-08-26 01:21:05 +0900

    studying commit

--- springboot/spring-reactor/src/main/java/com/test/springwebflux/SpringWebfluxApplication.java
+++ springboot/spring-reactor/src/main/java/com/test/springwebflux/SpringWebfluxApplication.java
@@ -29,11 +29,32 @@ public class SpringWebfluxApplication {
      *                     being created and delegated to worker threads to perform various operations like I/O
      *                     database processing, etc. While the worker threads are busy completing their processes, the servlet threads
      *                     enter a waiting state due to which the calls remain blocked. This is blocking or synchronous request processing.
-     *  
+     *
+     *  Non Blocking Request : In a non-blocking system, all the incoming requests are accompanied by an event handler
+     *                         and a callback. The request thread delegates the incoming request to a thread pool that
+     *                         manages a pretty small number of threads. Then the thread pool delegates the request to
+     *                         its handler function and gets availiable to process the next incoming requests from the request thread.
+     *                         When the handler function completes its process, one of the threads from the pool fetches the response and passes it to the callback function. Thus the threads in a non-blocking system never go into the waiting state. This increases the productivity and the performance of the application.
+     *                         A single request is potentially processed by multiple threads!
      */
 
+    /***
+     *
+     *  Back Pressure : Working with reactive code, we often across the term 'back pressure'. It is an analogy derived from fluid
+     *                  dynamics which literally means the resistence or force that opposes the desired flow of data.
+     * */
 
-
+    /**
+     *
+     *  Buffer - can easily buffer the deficit data and process it later when the server has capacity. But with
+     *           a huge load of dta coming in, this buffer might increase and the server would soon run out of memory.
+     *
+     *  Drop - Dropping, not processing eventsm should be the last option. Usually we can use the concept of data sampling
+     *         combined with buffering to achieve less data loss.
+     *
+     *  Control - The concept of controlling the producer that sends the data is by far the best option. Reactive Streams provides
+     *            various options in both push and pull-based streams to control the data that is being produced and sent to the consumer
+     * **/
 
     /**
      *  대부분 전통적인 Application 들은 Blocking Call 을 사용 하였다 (동기 적인 Call).
