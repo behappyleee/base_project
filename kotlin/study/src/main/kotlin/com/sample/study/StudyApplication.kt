3ede2b60932b2dbdff795bbaac32a471d2d7d559
behappyleee <dhfhfkwhdk@naver.com>
2023-09-04 22:37:50 +0900

    Kotlin Studying commit

--- kotlin/study/src/main/kotlin/com/sample/study/StudyApplication.kt
+++ kotlin/study/src/main/kotlin/com/sample/study/StudyApplication.kt
@@ -1,11 +1,22 @@
 package com.sample.study
 
+import com.sample.study.basic.KotlinClassTest
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import com.sample.study.basic.makeStr
+import com.sample.study.basic.makeStr2
 
 @SpringBootApplication
 class StudyApplication
 
 fun main(args: Array<String>) {
+	makeStr();
+	makeStr2();
+
+	val kotlinObjTest = KotlinClassTest();
+	kotlinObjTest.makeClassFunctionTest();
+	kotlinObjTest.makeClassFunctionTest2();
+
+
 	runApplication<StudyApplication>(*args)
 }
---
+++ kotlin/study/src/main/kotlin/com/sample/study/basic/KotlinFunctionTest.kt
@@ -0,0 +1,33 @@
+package com.sample.study.basic
+
+fun makeStr(): String {
+
+    println("THIS IS MAKE STR PRINTING");
+
+    return "THIS IS FRUNCTION STR FROM Kotlin Function";
+}
+
+fun makeStr2(): String {
+    println("THIS IS MAKE STR 2 METHOD CHECK");
+
+    return "THIS IS MAKE FUNCTION MAKE STR 2";
+}
+
+class KotlinClassTest {
+
+    fun makeClassFunctionTest(): String {
+        println("THIS IS MAKE CLASS FUNCTION TEST CHECL");
+
+        return "THIS IS METHOD FROM Kotlin Class Test Method";
+    }
+
+    fun makeClassFunctionTest2(): String {
+        println("THIS IS MAKE CLASS FUNCTION TEST 2 CHECK DATA TEST");
+
+        return "MAKE CLASS FUNCTION TEST CHECK !!";
+    }
+
+}
+
+
+
