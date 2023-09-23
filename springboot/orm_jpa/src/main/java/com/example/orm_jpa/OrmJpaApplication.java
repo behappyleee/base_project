20f905f7a250b8093777353a0cc1fde6751faaad
behappyleee <dhfhfkwhdk@naver.com>
2023-09-23 23:56:35 +0900

    studying commit

---
+++ springboot/orm_jpa/src/main/java/com/example/orm_jpa/OrmJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.orm_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmJpaApplication.class, args);
+    }
+
+}
