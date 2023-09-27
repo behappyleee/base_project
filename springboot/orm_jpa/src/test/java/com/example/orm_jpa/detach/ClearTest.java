cc5397c8e50abf0272dbae5fbf6081f0d5da0d5e
behappyleee <dhfhfkwhdk@naver.com>
2023-09-27 23:57:21 +0900

    jpa studying commit

---
+++ springboot/orm_jpa/src/test/java/com/example/orm_jpa/detach/ClearTest.java
@@ -0,0 +1,44 @@
+package com.example.orm_jpa.detach;
+
+import com.example.orm_jpa.example_1.domain.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.Test;
+
+public class ClearTest {
+
+    @Test
+    public void clearTest() {
+        // Entity Manager Factory 생성 (생성 비용이 아주 비싸다.)
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        // Entity Manager 생성 (생성 비용이 거의 안든다.)
+        EntityManager em = emf.createEntityManager();
+
+        // 회원 엔티티 생성, 비영속 상태
+        Member member = new Member();
+        member.setId("memberA");
+        member.setUsername("회원A");
+
+        Member member2 = new Member();
+        member2.setId("memberB");
+        member2.setUsername("회원B");
+
+        // 회원 엔티티 영속 상태
+        em.persist(member);
+        em.persist(member2);
+
+        // detach 가 엔티티 하나를 준영속 상태로 만들었다면 em.clear() 는 모든 엔티티 영속성을 초기화
+        em.clear();
+
+        // 트랜잭션 - 획득
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();     // 트랜잭션 시작
+            tx.commit();    // 트랜잭션 커밋 (해당 commit() 에서 실제로 INSERT 쿼리 가 작동을 함)
+        } catch(Exception e) {}
+
+    }
+
+}
---
+++ springboot/orm_jpa/src/test/java/com/example/orm_jpa/detach/CloseTest.java
@@ -0,0 +1,45 @@
+package com.example.orm_jpa.detach;
+
+import com.example.orm_jpa.example_1.domain.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.Test;
+
+public class CloseTest {
+
+    @Test
+    public void closeTest() {
+        // Entity Manager Factory 생성 (생성 비용이 아주 비싸다.)
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        // Entity Manager 생성 (생성 비용이 거의 안든다.)
+        EntityManager em = emf.createEntityManager();
+
+        // 회원 엔티티 생성, 비영속 상태
+        Member member = new Member();
+        member.setId("memberA");
+        member.setUsername("회원A");
+
+        Member member2 = new Member();
+        member2.setId("memberB");
+        member2.setUsername("회원B");
+
+        // 회원 엔티티 영속 상태
+        em.persist(member);
+        em.persist(member2);
+
+        // close 로 영속성 컨텍스트를 종료하면 해당 영속성 컨텍스트가 관리하던 영속 상태의 엔티티가 모두 준영속 상태가 됨
+        em.close();
+
+        // 트랜잭션 - 획득
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();     // 트랜잭션 시작
+            tx.commit();    // 트랜잭션 커밋 (해당 commit() 에서 실제로 INSERT 쿼리 가 작동을 함)
+        } catch(Exception e) {}
+
+
+    }
+
+}
