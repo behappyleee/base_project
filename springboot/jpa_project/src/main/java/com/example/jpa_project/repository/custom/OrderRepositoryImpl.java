3824b4676a037cf004bc335bdac5141eefa76f46
behappyleee <dhfhfkwhdk@naver.com>
2023-10-08 23:57:23 +0900

    jpa edit commit

--- springboot/jpa_project/src/main/java/com/example/jpa_project/repository/custom/OrderRepositoryImpl.java
+++ springboot/jpa_project/src/main/java/com/example/jpa_project/repository/custom/OrderRepositoryImpl.java
@@ -1,11 +1,11 @@
 package com.example.jpa_project.repository.custom;
 
-import com.mysema.query.jpa.JPQLQuery;
+// import com.mysema.query.jpa.JPQLQuery;
 
 import com.example.jpa_project.domain.Order;
 import com.example.jpa_project.domain.OrderSearch;
-import com.example.jpa_project.domain.QMember;
-import com.example.jpa_project.domain.QOrder;
+//import com.example.jpa_project.domain.QMember;
+//import com.example.jpa_project.domain.QOrder;
 
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.util.StringUtils;
@@ -27,21 +27,25 @@ public class OrderRepositoryImpl extends QuerydslRepositorySupport implements Cu
 
     @Override
     public List<Order> search(OrderSearch orderSearch) {
-        QOrder order = QOrder.order;
-        QMember member = QMember.member;
+        // TODO
+        // Error 해결 하기 !!
 
+        return null;
 
-        JPQLQuery query = from(order);
-
-        if (StringUtils.hasText(orderSearch.getMemberName())) {
-            query.leftJoin(order.member, member)
-                    .where(member.name.contains(orderSearch.getMemberName()));
-        }
-
-        if (orderSearch.getOrderStatus() != null) {
-            query.where(order.status.eq(orderSearch.getOrderStatus()));
-        }
-
-        return query.list(order);
+//        QOrder order = QOrder.order;
+//        QMember member = QMember.member;
+//
+//        JPQLQuery query = from(order);
+//
+//        if (StringUtils.hasText(orderSearch.getMemberName())) {
+//            query.leftJoin(order.member, member)
+//                    .where(member.name.contains(orderSearch.getMemberName()));
+//        }
+//
+//        if (orderSearch.getOrderStatus() != null) {
+//            query.where(order.status.eq(orderSearch.getOrderStatus()));
+//        }
+//
+//        return query.list(order);
     }
 }
