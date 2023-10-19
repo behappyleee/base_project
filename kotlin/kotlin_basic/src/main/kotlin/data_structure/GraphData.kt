277d59afa40059a4992e750f927db6d174f6ac7b
Chul Han Lee <dhfhfkwhdk@naver.com>
2023-10-19 23:58:43 +0900

    kotlin studying commit

---
+++ kotlin/kotlin_basic/src/main/kotlin/data_structure/GraphData.kt
@@ -0,0 +1,9 @@
+package data_structure
+
+fun main() {
+
+    // Non-Linear Data Structure
+    // Graph - A Graph is a non-linear data structure consisting of vertices and edges. The vertices are
+    // sometimes also reffered to as nodes and the edges are lines or arcs that connect any two nodes in the graph.
+    
+}
