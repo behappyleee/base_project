2b93a89fa30dba42306d8388a590822101147e46
Chul Han Lee <dhfhfkwhdk@naver.com>
2023-11-02 00:24:49 +0900

    test 1

--- kotlin/study/src/main/kotlin/com/sample/study/basic/CoroutineTest.kt
+++ kotlin/study/src/main/kotlin/com/sample/study/basic/CoroutineTest.kt
@@ -1,9 +1,11 @@
 package com.sample.study.basic
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 fun main() {
-
     // Coroutine 은 다양한 태스크를 진행할 때 필요한 요소
     // Thread 와 비슷하지만 Thread 와 다른 점은
 
@@ -13,11 +15,15 @@ fun main() {
     // 3. 코루틴을 사용하면 작업을 쉽게 전환하며 스레드를 옮겨다니며 작업할 수 있다
     // 4. 효율적으로 처리 속도도 빠름
 
+    // Network 에서 불러올 데이터
+    GlobalScope.launch(Dispatchers.IO){
+
+
+    }
 
 }
 
 fun coroutineFun() {
-
     // Coroutine 을 사용하여 Delay 사용 하기
     // Coroutine 을 실행 하는 가장 기본적인 방법은 Global Scope 을 이용 하는 거다.
     // Job 은 백 그라운드 작업을 의미
