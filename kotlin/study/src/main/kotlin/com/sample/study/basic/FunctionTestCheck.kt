958f3d339a8aeb7b35f375ae8cdbd24ec5c4a00f
Chul Han Lee <dhfhfkwhdk@naver.com>
2023-11-10 00:51:47 +0900

    studying commit

---
+++ kotlin/study/src/main/kotlin/com/sample/study/basic/FunctionTestCheck.kt
@@ -0,0 +1,11 @@
+package com.sample.study.basic
+
+val existsOver5 = {n: FunctionTest -> n.age > 5}
+class FunctionTest (var age:Int)
+val numbers = listOf(1, 2, 3, 4, 5, 6);
+
+fun main() {
+
+    val numbers = listOf(1, 2, 3, 4, 5, 6);
+
+}
--- kotlin/study/src/main/kotlin/com/sample/study/type/DataClassType.kt
+++ kotlin/study/src/main/kotlin/com/sample/study/type/DataClassType.kt
@@ -1,12 +1,43 @@
 package com.sample.study.type
 
-class DataClassType {
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.util.*
 
+fun main() {
+    val calendarInstance = Calendar.getInstance()
+    val todayDateTime: LocalDateTime = LocalDateTime.now()
+    val todayDate: LocalDate = LocalDate.now()
 
+    println(todayDateTime)
+    println(todayDate)
 
+    val zoneId = ZoneId.of("")
 
 
 }
+
+
+
+class DataClassType {
+
+    var t1 = StudentData("test1", 3);
+    val calInstance = Calendar.getInstance();
+
+
+}
+
+
+
+
+fun testFun(param: StudentData) {
+
+}
+
 
 data class StudentData (val name: String, val age: Int) {
 
