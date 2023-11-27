f9e336b322369f2531e735131bf7c4715e82d6f5
behappyleee <72903623+behappyleee@users.noreply.github.com>
2023-11-27 22:41:27 +0900

    studying commit

--- kotlin/kotlin-guide/src/main/kotlin/practice/grammar/Map_HashMap_Kotlin.kt
+++ kotlin/kotlin-guide/src/main/kotlin/practice/grammar/Map_HashMap_Kotlin.kt
@@ -9,6 +9,11 @@ fun main(args: Array<String>) {
     myMap.put(7, "Test")
     myMap.put(2, "Check")
 
+    // myMap to 도 사용이 가능
+    val aa = mapOf(2 to "check11", "test11" to 3)
+
+    println(aa)
+
     for(key in myMap.keys) {
         println("Map Element : KEY : $key + ${myMap[key]}")
     }
