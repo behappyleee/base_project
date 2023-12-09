ca07365d38bb9c4ebb9adbbb2c5642106370e0cf
behappyleee <72903623+behappyleee@users.noreply.github.com>
2023-12-09 15:51:07 +0900

    studying commit

---
+++ kotlin/study/src/main/kotlin/com/sample/study/test/controller/TestStudyController.kt
@@ -0,0 +1,27 @@
+package com.sample.study.test.controller
+
+import com.sample.study.test.service.TestStudyService
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestStudyController(
+    val service: TestStudyService,
+) {
+
+    val logger = LoggerFactory.getLogger(TestStudyController::class.java)
+
+    @GetMapping("/api/parameter/encoding/{pathEncoding}")
+    fun testStudy(
+        @PathVariable pathEncoding: String,
+    ): String {
+        logger.debug("TEST STUDYING CHECK PATH ENCODING : {} ", pathEncoding)
+        service.studyTest()
+
+        return "This is Test Controller !"
+    }
+
+
+}
---
+++ kotlin/study/src/main/kotlin/com/sample/study/test/service/TestStudyService.kt
@@ -0,0 +1,13 @@
+package com.sample.study.test.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class TestStudyService {
+
+    fun studyTest(): String {
+
+        return "Test Studying !!"
+    }
+
+}
