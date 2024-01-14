43b708b010fc41572c3a245082399ae72ff54f44
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-01-14 23:57:28 +0900

    studying commit

---
+++ kotlin/study/src/main/kotlin/com/sample/study/jwt/JwtTest.kt
@@ -0,0 +1,23 @@
+package com.sample.study.jwt
+
+import io.netty.handler.codec.serialization.ObjectEncoderOutputStream
+import java.util.Base64.Encoder
+
+fun main() {
+
+    fun jwtTest() {
+        val secretKey = "TestSecret"
+    }
+
+    fun encodeKey(seceretKey: String): String {
+        return "String Test"
+    }
+
+}
+
+class JwtTest {
+
+
+
+
+}
