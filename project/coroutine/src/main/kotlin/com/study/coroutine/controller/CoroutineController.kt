1d1f5db5addf04651dd1f940e81892df63339256
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-01-29 23:58:15 +0900

    studying commit

--- project/coroutine/src/main/kotlin/com/study/coroutine/controller/CoroutineController.kt
+++ project/coroutine/src/main/kotlin/com/study/coroutine/controller/CoroutineController.kt
@@ -1,6 +1,16 @@
 package com.study.coroutine.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
 class CoroutineController {
 
+    @GetMapping("/hello")
+    fun sayHello(): String {
+        return "Hello, Spring Boot with Kotlin !"
+    }
 
 }
