f8a015d9b21e777fc7926033d366b4305f4fbf2f
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-01-31 00:25:17 +0900

    studying commit

---
+++ project/jwt/src/main/java/com/study/jwt/entity/Authority.kt
@@ -0,0 +1,16 @@
+package com.study.jwt.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "authority")
+data class Authority(
+
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private val authorityName: String,
+
+)
--- project/jwt/src/main/java/com/study/jwt/entity/User.kt
+++ project/jwt/src/main/java/com/study/jwt/entity/User.kt
@@ -2,10 +2,19 @@ package com.study.jwt.entity
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.Column
+import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
+import jakarta.persistence.ManyToMany
+import jakarta.persistence.Table
 
+// Kotlin 에서 JPA 사용하기 위하여서는 kotlin.plugin.jpa plugin 추가가 필요 !
+// Springboot 시작 시 resource 밑에 data.sql 이 자동실행이 됨 !
+@Entity
+@Table(name = "user")
 data class User (
     @JsonIgnore
     @Id
@@ -23,9 +32,17 @@ data class User (
     @Column(name = "nickname", length = 50)
     private val nickName: String,
 
-    
+    @JsonIgnore
+    @Column(name = "activated")
+    private val activated: Boolean,
 
+    // ManyToMany Join 테이블 어노테이션은 회원 객체와 권한 객체를 1:다 다:1 관계를 서로 맵핑
+    @ManyToMany
+    @JoinTable(
+        name = "user_authority",
+        joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "user_id")],
+        inverseJoinColumns = [JoinColumn(name="authority_name", referencedColumnName = "authority_name")]
+    )
+    private val authorities: Set<Authority>
+)
 
-) {
-
-}
