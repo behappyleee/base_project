ebbda5349f08a24ee3b86014a7c4126ce98ff347
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-02-26 00:58:42 +0900

    studying commit

---
+++ project/cleancode/src/main/java/com/book/cleancode/chpt_1/CleanCode_1.java
@@ -0,0 +1,17 @@
+package com.book.cleancode.chpt_1;
+
+
+public class CleanCode_1 {
+
+    public static void main(String[] args) {
+        
+        // Clean 코드의 가장 중요한 점
+        // 1. 중복 줄이기
+        // 2. 시스템 내 모든 설께 아이디어를 표현
+        // 3. 클래스, 메서드 함수 등을 최대한 줄임
+
+        // 보이 스카우트 규칙 ====> 체크아웃 할 때 보다 조금 더 꺠끗한 코드를 체크인 한다면 코드는 절대 나빠지지 않는다
+
+    }
+
+}
---
+++ project/cleancode/src/test/java/com/book/cleancode/CleancodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.cleancode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleancodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
