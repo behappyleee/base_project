a73b1170f97ec2972665e607288febc912f8daf8
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-03-10 15:23:59 +0900

    studying commit

--- project/kotlin_book/src/main/kotlin/com/example/kotlin_book/chapter_6/KotlinData_Expression.kt
+++ project/kotlin_book/src/main/kotlin/com/example/kotlin_book/chapter_6/KotlinData_Expression.kt
@@ -9,6 +9,7 @@ class KotlinData_Expression {
 }
 
 fun main() {
+    println(Double.NaN) // Return 값 NaN
 
     val mapTest = emptyMap<String, Int>()
     val test = mapTest["TEST"]
@@ -23,5 +24,24 @@ fun main() {
 
 }
 
+fun mean(list: List<Int>): Double = when {
+    list.isEmpty() -> Double.NaN    // List 가 비었을 시 ... !
+    else -> list.sum().toDouble()   // list.size
+}
 
+// 다른 방법 !
+fun mean1(list: List<Int>): Double = when {
+    // 또 다른 방법으로는 그냥 예외를 던져 버리는 것이다.
+    // 하지만 해당 방법은 더 많은 문제를 일으킨다. 입력이 없어서 결과가 없는 것 뿐이다 !
+    // 예외를 추가하면 함수가 더 이상 순수 함수가 아니게 된다. 그리고 다른 함수와 더 이상 호환이 안된다.
+    // null 을 반환하고 호출 하는 쪽에서 null 처리를 할 수 있다. 일반적으로 언어에서 null 을 반환하는 것은 가장 나쁜 해법이다.
+    list.isEmpty() -> throw IllegalArgumentException("This is Exception !")
+    else -> list.sum().toDouble()
+}
 
+// 또 다른 방법은 default 수를 받는 것이다 ! list 가 없을 거나 대비해서 이다 !
+fun maxWithDefault(list: List<Int>, defaultNum: Int): Int = when {
+    list.isEmpty() -> defaultNum
+    else -> list.max()  // 리스트가 비어 있으면 defaultNum 을 주는데 문제는 list 에 0 만 있거나
+    // 진짜 최댓값이 0 일때가 문제이다 !
+}
---
+++ project/kotlin_book/src/main/kotlin/com/example/kotlin_book/chapter_6/Kotlin_Option.kt
@@ -0,0 +1,60 @@
+package com.example.kotlin_book.chapter_6
+
+import java.util.Optional
+
+class Kotlin_Option {
+
+
+
+}
+
+fun main() {
+
+}
+
+
+// KotlinOptionTest 는 A 에 대하여 공변 적이다 !
+// Option 과 List 는 매우 유사하다. 둘 다 추상 클래스이며 두 가 지 구현이 들어있다.
+sealed class KotlinOptionTest <out A> {
+    abstract fun isEmpty(): Boolean
+
+    internal object None: KotlinOptionTest<Nothing>() {  // None 은 모든 타입에 대해 사용할 수 있는 싱글톤 객체
+
+        override fun isEmpty(): Boolean {
+            return true
+        }
+
+        override fun toString(): String {
+            return "None !"
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return other === None   // 데이터가 없는 경우 모두 다 같다고 취급
+        }
+
+        override fun hashCode(): Int {
+            return 0
+        }
+    }
+
+
+    // Some 의 인스턴스의 경ㅇ value 에 저장 된 값이 같은 경우에만 동등하며 데이터 클래스이기 때문에 이런 동등성 비교
+    // 함수를 컴파일러가 자동으로 제공
+    internal data class Some <out A>(internal val value: A):
+        KotlinOptionTest<A>() {
+        override fun isEmpty(): Boolean {
+            return true
+        }
+    }
+
+    companion object {
+        // Operator 키워드로 invoke 함수를 정의 !
+        operator fun <A> invoke(a: A? = null): KotlinOptionTest<A> {
+            return when(a) {
+                null -> None
+                else -> Some(a)
+            }
+        }
+    }
+
+}
