f4ae74af02ea87db7c557ef649bd48e15936a511
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-06-03 00:12:42 +0900

    studying commit

--- java_test/src/socket/ChatSocketClient.java
+++ java_test/src/socket/ChatSocketClient.java
@@ -1,8 +1,26 @@
 package socket;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
+
+
 public class ChatSocketClient {
 
+    private static final Logger logger = Logger.getLogger(ChatSocketClient.class.getName());
 
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(new InputStream() {
+            @Override
+            public int read() throws IOException {
+                return 201;
+            }
+        }));
+        String aa = br.readLine();
+        System.out.println("[PRINT ... ] " + aa);
 
+    }
 }
