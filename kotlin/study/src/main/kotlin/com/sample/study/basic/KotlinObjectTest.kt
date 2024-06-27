00a461d106fbca54452ae925e5977ec9b1afcd90
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-06-28 00:14:29 +0900

    studying commit

--- kotlin/study/src/main/kotlin/com/sample/study/basic/KotlinObjectTest.kt
+++ kotlin/study/src/main/kotlin/com/sample/study/basic/KotlinObjectTest.kt
@@ -1,4 +1,48 @@
 package com.sample.study.basic
 
 class KotlinObjectTest {
+
+}
+
+data class KotlinUser(
+    val name: String,
+    var age: Int,
+)
+
+class KotlinUserClass {
+    var name: String = ""
+        get() = field
+        set(value) {
+            field = value + "SET VALYE !!"
+        }
+    var age: Int = 21
+}
+
+fun <T> T.makeObject(block: T.() -> Unit): T {
+    this.block()
+
+    return this
+}
+
+fun main() {
+
+    val aaa = KotlinUser(name = "NAME11", age = 22).makeObject {
+        this.name
+        this.age = 17
+    }
+    println(aaa)
+
+    aaa.run {  }
+    aaa.let {  }
+
+    val lll = listOf(1, 2, 3, 4)
+    lll.map {  }
+    lll.apply {  }
+
+
+    val ccc = KotlinUserClass().makeObject {
+        this.name = "Make Name !!"
+        this.age = 22
+    }
+    println(ccc.name + "  " + ccc.age)
 }
