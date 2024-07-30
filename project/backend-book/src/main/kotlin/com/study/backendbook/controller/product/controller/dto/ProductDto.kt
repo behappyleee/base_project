a24ac0d181f0fe5dcf1cf45861575e5ae94a52e9
behappyleee <dhfhfkwhdk@naver.com>
2024-07-31 07:56:28 +0900

    comment commit

--- project/backend-book/src/main/kotlin/com/study/backendbook/controller/product/controller/dto/ProductDto.kt
+++ project/backend-book/src/main/kotlin/com/study/backendbook/controller/product/controller/dto/ProductDto.kt
@@ -1,6 +1,7 @@
 package com.study.backendbook.controller.product.controller.dto
 
 import com.study.backendbook.controller.product.domain.Product
+import java.util.Vector
 
 // 실무에서는 DTO 가 자주 변경 되기에 표현 계층에서 사용 되는 데이터 클래스와 서비스에서 사용되는 데이터 클래스를 분리 시키는게 낫다
 // DTO 는 표현 계층 부터 어플리케이션 계층 까지 역할을 하고 그 안쪽까지는 전달이 되지는 않는다.
