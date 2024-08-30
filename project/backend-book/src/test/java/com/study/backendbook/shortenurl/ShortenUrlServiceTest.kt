275132f426066a092fbf42963d8e25e46cc881e7
behappyleee <dhfhfkwhdk@naver.com>
2024-08-30 13:37:49 +0900

    studying commit

--- project/backend-book/src/test/java/com/study/backendbook/shortenurl/ShortenUrlServiceTest.kt
+++ project/backend-book/src/test/java/com/study/backendbook/shortenurl/ShortenUrlServiceTest.kt
@@ -1,4 +1,35 @@
 package com.study.backendbook.shortenurl
 
-class ShortenUrlServiceTest {
+import com.study.backendbook.shorturl.application.ShortenUrlService
+import com.study.backendbook.shorturl.presentation.dto.ShortenUrlCreateRequest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShortenUrlServiceTest(
+    @Autowired
+    private val shortenUrlService: ShortenUrlService,
+) {
+    @Test
+    @DisplayName(value = "URL 을 단축 URL 로 단축 한 후 기존 URL 이 조회 되어야 한다.")
+    fun shortenUrlTest() {
+        val expectedOriginalUrl = "https://www.hanbit.co.kr"
+        val shortenUrlRequestDto =
+            ShortenUrlCreateRequest(
+                originalUrl = expectedOriginalUrl,
+            )
+
+        val shortenUrlResponse= shortenUrlService.generateShortenUrl(shortenUrlCreateRequest = shortenUrlRequestDto)
+        val shortenUrlKey = shortenUrlResponse.shortenUrlKey
+
+        val originalUrl =
+            shortenUrlService.getOriginalUrlByShortenUrlKey(
+                shortenUrlKey = shortenUrlKey,
+            )
+
+        Assertions.assertTrue { expectedOriginalUrl == originalUrl }
+    }
 }
