8150c57888ac0784069ecfba1b460c68704cb793
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-10-05 23:43:51 +0900

    studying commit

---
+++ data-structure/src/HashStudy.java
@@ -0,0 +1,10 @@
+public class HashStudy {
+    public static void main(String[] args) {
+
+        // Hash 법은 검색과 더불어서 데이터의 추가와 삭제도 효율적으로 수행할 수 있는 작업
+        // 해시법은 데이터를 저장할 위치 (인덱스)를 간단한 연산으로 구하는 것, 검색 뿐만 아니라 추가, 삭제도 효율적으로 가능
+
+
+
+    }
+}
