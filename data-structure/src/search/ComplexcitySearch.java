555c6bab123853acdf72f5120cdc46b6d76df0d3
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-10-06 22:04:13 +0900

    studying commit

--- data-structure/src/search/ComplexcitySearch.java
+++ data-structure/src/search/ComplexcitySearch.java
@@ -1,5 +1,7 @@
 package search;
 
+import java.util.Arrays;
+
 public class ComplexcitySearch {
     public static void main(String[] args) {
         // 복잡도 - 프로그램의 실행 속도는 프로그램이 동작하는 하드웨어나 컴파일러 등의 조건에 따라 달라짐, 알고리즘의 성능을 객관적으로 평가하는
@@ -7,7 +9,12 @@ public class ComplexcitySearch {
 
         // 시간 복잡도 - 실행에 필요한 시간을 평가한 것
         // 공간 복잡도 - 기억 영역과 파일 공간이 얼마나 필요한가를 평가한 것
+        int[] test = new int[3];
+        test[0] = 1;
+        test[1] = 8;
 
+        int testInt = Arrays.binarySearch(test, 7);
+        System.out.println(testInt);
 
     }
 }
