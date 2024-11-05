d8453c28610927867ee4afd40757fd24f0581bd0
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-11-06 00:31:21 +0900

    study commit

---
+++ springboot-board/src/main/kotlin/com/study/springbootboard/SpringbootBoardApplication.kt
@@ -0,0 +1,11 @@
+package com.study.springbootboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootBoardApplication>(*args)
+}
