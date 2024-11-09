818523ee3d48f702a7119a26a8af8cb3252262e8
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-11-09 23:55:12 +0900

    study commit

---
+++ springboot-board/src/main/kotlin/com/study/springbootboard/service/TestCheck.java
@@ -0,0 +1,11 @@
+package com.study.springbootboard.service;
+
+public class TestCheck {
+
+    public static void main(String[] args) {
+
+        // java 파일에서 class 파일을 만드는 명령어 javac TestCheck
+        // javap -v {클래스 파일 이름} ====> 클래스 파일을 역 어셈블리하여 볼 수 잇음 (클래스 파일 먼저 생성하여야 함)
+
+    }
+}
---
+++ springboot-board/src/main/kotlin/com/study/springbootboard/service/TestService.kt
@@ -0,0 +1,8 @@
+package com.study.springbootboard.service
+
+
+class TestService {
+
+    fun add(a: Int, b: Int): Int = a + b
+
+}
