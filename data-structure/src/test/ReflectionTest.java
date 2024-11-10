eeb5413c86a54009706329dafe04baa7731c99e6
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-11-10 14:02:19 +0900

    study commit

---
+++ data-structure/src/test/ReflectionTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public class ReflectionTest {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException {
+        Class<TestClass> clazz = TestClass.class;
+        Field field = clazz.getDeclaredField("privateName");
+
+        Constructor<TestClass> aaa = clazz.getDeclaredConstructor(String.class, Integer.class, Array.class);
+        var asdasd= aaa.newInstance("TEST CHECK", 3, new String[] {"A", "B", "C"});
+
+        TestClass testClass = new TestClass();
+
+        // setAccessible 을 true 로 변경 시 private 데이터 필드에도 접근이 가능하게 해 준다.
+        field.setAccessible(true);
+        var privateName = field.get(testClass);
+
+        System.out.println("PRIVATE NAME : " + privateName);
+        System.out.println("Field Name : " + field.getName());
+        System.out.println("Field Value : ");
+    }
+}
+
+
+class TestClass {
+
+    private final String privateName = "This is Private Name !";
+}
