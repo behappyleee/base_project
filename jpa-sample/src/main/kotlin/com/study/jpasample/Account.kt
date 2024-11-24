d0276efb5a06b4d790aa37419eb3b4e9b76057a4
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-11-24 21:16:32 +0900

    studying commit

--- jpa-sample/src/main/kotlin/com/study/jpasample/Account.kt
+++ jpa-sample/src/main/kotlin/com/study/jpasample/Account.kt
@@ -1,24 +1,53 @@
 package com.study.jpasample
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
-class Account {
-
+class Account(
+    // TODO : JPA 를 코틀린에서 사용할 떄 BEST Practice 를 고민해 보기 ... !
+    // JPA 는 반드시 No-Args 생성자를 가져야 한다.
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private var id: Long
-        get() {
-            TODO()
-        }
-        set(value) {
-            field = value
-        }
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column
+    val userName: String= "",
+    @Column
+    val password: String = "",
+) {}
 
-    private lateinit var name: String
 
-    private lateinit var password: String
-}
+//@Entity
+////@Table(name = "ACCOUNT")
+//class Account {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    var id: Long? = null
+//
+//    @Column
+//    var userName: String? = null
+////    var name: String
+////
+////    private lateinit var password: String
+//}
+//
+//fun main() {
+//
+//    data class Test(
+//        val name: String,
+//    ) {
+//        var age:Int = 0
+//    }
+//
+//    val t1 = Test("1")
+//    t1.age = 10
+//
+//    val t2 = Test("1")
+//    t2.age = 20
+//
+//    println(t1 == t2)
+//}
