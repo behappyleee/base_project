c966ff3eed3559e55663d0dcbc621f4b3cdb054a
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-05-18 17:14:05 +0900

    commit

--- oop/src/main/java/templatemethod/data/CSVDataProcessor.java
+++ oop/src/main/java/templatemethod/data/CSVDataProcessor.java
@@ -1,9 +1,34 @@
 package templatemethod.data;
 
+import com.sun.jdi.event.ThreadDeathEvent;
+
+import javax.swing.text.html.Option;
+import java.nio.channels.Selector;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Predicate;
+
 // CSV 데이터를 처리하는 과정을 구현
 public class CSVDataProcessor extends DataProcessor {
     @Override
     protected void loadData(String data) {
+        Predicate<String> aaaaa = (test) -> test.length() > 1;
+        aaaaa.and((test) -> test.length() > 3);
+        var aaaa = aaaaa.test("TEST");
+
+        Predicate<String> a1 = (test) -> test.length() > 1;
+        Predicate<String> a2 = (test) -> test.length() > 3;
+        Predicate<String> a3 = (test) -> test.length() > 4;
+        var a4 = a1.and(a2).and(a3);
+
+        ReentrantLock lock = new ReentrantLock();
+        Semaphore semaphore = new Semaphore(1);
+        lock.tryLock();
         System.out.println("Loading data from CSV ....");
     }
 
@@ -20,5 +45,18 @@ public class CSVDataProcessor extends DataProcessor {
     @Override
     protected boolean isVisibleData(String data) {
         return data != null && data.contains("CSV");
+    }
+
+    public static void main(String[] args) {
+        Long startTime = System.currentTimeMillis();
+        final int COUNT = 100_000;
+
+        for (int i = 0; i < COUNT; i++) {
+            Thread thread = new Thread(() -> {});
+            thread.start();
+        }
+
+        Long endTime = System.currentTimeMillis();
+        System.out.println("소요 시간 NanoSeconds : " + (endTime - startTime));
     }
 }
--- refactor/src/main/kotlin/after/Test.kt
+++ refactor/src/main/kotlin/after/Test.kt
@@ -2,8 +2,150 @@ package after
 
 import before.GildedRose
 import before.Item
+import com.sun.org.slf4j.internal.LoggerFactory
+
+sealed class FlowerItem(
+    protected open val name: String,
+    protected open var sellIn: Int,
+    protected open var quality: Int,
+    protected open val controlFn: (Int) -> Int = {
+        when {
+            quality <= 0 -> quality
+            sellIn > 0 -> quality - DEFAULT_DISCOUNT_QUALITY
+            sellIn <= 0 -> quality - (DEFAULT_DISCOUNT_QUALITY * 2)
+            else ->  quality - DEFAULT_DISCOUNT_QUALITY
+        }
+    },
+): after.Item {
+
+    data class NormalFlower(
+        override val name: String = "Normal Flower",
+        override var sellIn: Int = 50,
+        override var quality: Int = 30,
+    ): FlowerItem(
+        name = name,
+        sellIn = sellIn,
+        quality = quality
+    ) {
+        override fun controlQuality() {
+            controlCustomQuality(
+                controlQuality = DEFAULT_DISCOUNT_QUALITY,
+                controlFn = this.controlFn,
+            )
+        }
+    }
+
+    data class Sulfuras(
+        override val name: String = "Sulfuras",
+        override var sellIn: Int = 50,
+    ): FlowerItem(
+        name = name,
+        sellIn = sellIn,
+        quality = SULFURAS_QUALITY
+    ) {
+        override fun controlQuality() {
+            logger.warn("Sulfuras Flower Quality can't control !")
+        }
+
+        companion object {
+            private const val SULFURAS_QUALITY: Int = 80
+        }
+    }
+
+    data class AgedBrie(
+        override val name: String = "Aged Brie",
+        override var sellIn: Int = 50,
+        override var quality: Int = 30,
+    ): FlowerItem(
+        name = name,
+        sellIn = sellIn,
+        quality = quality
+    ) {
+        override fun controlQuality() {
+            controlCustomQuality(
+                controlQuality = DEFAULT_DISCOUNT_QUALITY,
+            ) {
+                quality + it
+            }
+        }
+    }
+
+    data class Conjured(
+        override val name: String = "Conjured",
+        override var sellIn: Int = 50,
+        override var quality: Int = 20,
+    ): FlowerItem(
+        name = name,
+        sellIn = sellIn,
+        quality = quality
+    ) {
+        override fun controlQuality() {
+            controlCustomQuality(
+                controlQuality = if (quality >= DEFAULT_DISCOUNT_QUALITY * 2) {
+                    DEFAULT_DISCOUNT_QUALITY * 2
+                } else {
+                    DEFAULT_DISCOUNT_QUALITY
+                },
+            ) {
+                quality - it
+            }
+        }
+    }
+
+    data class BackStage(
+        override val name: String,
+        override var sellIn: Int,
+        override var quality: Int,
+    ): FlowerItem(
+        name = name,
+        sellIn = sellIn,
+        quality = quality,
+    ) {
+        override fun controlQuality() {
+            controlCustomQuality(
+                controlQuality = when {
+                    sellIn  in 1.. 5 -> DEFAULT_DISCOUNT_QUALITY * 3
+                    sellIn in 6..10 -> DEFAULT_DISCOUNT_QUALITY * 2
+                    sellIn > 10 -> DEFAULT_DISCOUNT_QUALITY
+                    else -> quality
+                }
+            ) {
+                if (sellIn > 0) {
+                    quality + it
+                } else {
+                    quality - it
+                }
+            }
+        }
+    }
+
+    fun controlCustomQuality(
+        controlQuality: Int,
+        controlFn: (Int) -> Int,
+    ): Int {
+        require(
+            quality <= MAX_QUALITY || quality >= MINIMUM_QUALITY
+        ) {
+            logger.error("Quality Should Not Over 50 or Negative")
+            return quality
+        }
+        return controlFn(controlQuality)
+    }
+
+    companion object {
+        private const val MAX_QUALITY = 50
+        private const val MINIMUM_QUALITY = 0
+        private const val DEFAULT_DISCOUNT_QUALITY = 1
+        private val logger = LoggerFactory.getLogger(FlowerItem::class.java)
+    }
+}
+
+
 
 fun main(args: Array<String>) {
+    val items = listOf(
+       FlowerItem.AgedBrie(name = "Aged Brie", sellIn = 50),
+    )
 
 //    println("OMGHAI!")
 //
