0fb62a862515082f71dcb8d4071bd55df26d86d0
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-07-27 03:24:25 +0900

    feat: add server response

--- code-craft/src/main/kotlin/httpserver/MyHttpServer.kt
+++ code-craft/src/main/kotlin/httpserver/MyHttpServer.kt
@@ -5,6 +5,17 @@ import java.net.ServerSocket
 fun main(args: Array<String>) {
     println("Server starting...")
     val httpServer = ServerSocket(4221)
-    httpServer.accept()
+
+    httpServer.reuseAddress = true
+    val client = httpServer.accept()
+    println("Accepted New Connection !")
+
+    val output = client.outputStream
+
+    output.write("HTTP/1.1 200 OK This is Custom !\r\n\r\n".toByteArray())
+    output.flush()
+
+    output.close()
+    client.close()
     println("Server stopped.")
 }
