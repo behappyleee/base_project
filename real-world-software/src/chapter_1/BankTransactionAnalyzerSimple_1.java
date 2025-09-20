6e534677ffa394011bcc628fa33e04cdef33ce8a
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-09-21 01:51:54 +0900

    feat: add sqs test repo

--- real-world-software/src/chapter_1/BankTransactionAnalyzerSimple_1.java
+++ real-world-software/src/chapter_1/BankTransactionAnalyzerSimple_1.java
@@ -9,10 +9,10 @@ import java.util.List;
 public class BankTransactionAnalyzerSimple_1 {
 
     // 간단한 csv 파일을 파싱하는 클래스
-    private static final String RESOURCES = "src/main/resources/";
+    private static final String RESOURCES = "src/chapter_1/";
 
     public static void main(String[] args) throws IOException {
-        final Path path = Paths.get(RESOURCES + "transactions.txt");
+        final Path path = Paths.get(RESOURCES + "/transactions.csv");
         final List<String> lines = Files.readAllLines(path);
         double total = 0d;
         for (final String line : lines) {
--- real-world-software/src/chapter_1/BankTransaction_2.java
+++ real-world-software/src/chapter_1/BankTransaction_2.java
@@ -4,21 +4,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransaction_2 {
 
     // 간단한 csv 파일을 파싱하는 클래스
-    private static final String RESOURCES = "src/chapter_1/transactions.txt";
+    private static final String RESOURCES = "src/chapter_1/";
 
     public static void main(String[] args) throws IOException {
-        final Path path = Paths.get(RESOURCES + "transactions.txt");
+        final Path path = Paths.get(RESOURCES + "transactions.csv");
         final List<String> lines = Files.readAllLines(path);
         double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        // 1월 입출금 내역 합계 계산하기
         for (final String line : lines) {
             final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[0]);
-            total += amount;
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
         }
 
         System.out.println("The total for all transaction is " + total);
